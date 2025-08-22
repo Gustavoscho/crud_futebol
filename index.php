2164943f90a1c8065c7cb6f1a9b5c1bc8e17b1ad
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-08-22 11:50:43 -0300

    add css

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
 </head>
 <body>
     <h1>O que quer fazer?</h1>
-    <a href="create_jogador.php">Cadastrar jogador</a>
     <a href="create_time.php">Cadastrar time</a>
+    <a href="create_jogador.php">Cadastrar jogador</a>
     <a href="create_partida.php">Cadastrar partida</a>
+    <br><br>
     <a href="read_time.php">Listar times</a>
     <a href="read_jogador.php">Listar jogador</a>
     <a href="read_partida.php">Listar partidas</a>
