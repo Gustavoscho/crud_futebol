0ecd86a85b7f1814a278b56eea72af423dca8cfd
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-08-22 12:32:56 -0300

    fix

--- create_time.php
+++ create_time.php
@@ -6,13 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $nome = $_POST['nome'];
     $cidade = $_POST['cidade'];
+    $sql = "INSERT INTO times (nome,cidade) VALUE ('$nome','$cidade')";
 
-    $sql = " INSERT INTO times (nome,cidade) VALUE ('$nome','$cidade')";
-
-    if ($conn->query($sql) === true) {
-        echo "Novo time criado com sucesso.";
-    } else {
-        echo "Erro " . $sql . '<br>' . $conn->error;
+    if( $conn->query($sql) === true){
+        echo"Novo regristro criado com sucesso.";
+    } else{
+        echo"Erro".$sql.'<br>'.$conn->error;
     }
     $conn->close();
 }
@@ -26,23 +25,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create_time</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <form method="POST" action="create_time.php">
 
-        <label for="name">Nome:</label>
-        <input type="text" name="name" required>
+        <label for="name" id="labels">Nome:</label>
+        <input type="text" name="name" required id="inputs">
 
-        <label for="cidade">Cidade:</label>
-        <input type="text" name="cidade" required>
+        <label for="cidade" id="labels">Cidade:</label>
+        <input type="text" name="cidade" required id="inputs">
 
         <input type="submit" value="Adicionar">
 
     </form>
 
-    <a href="index.php">Voltar para página inicial</a>
+    <a href="index.php" id="link">Voltar para página inicial</a>
 
 </body>
 
--- index.php
+++ index.php
@@ -3,15 +3,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicial</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>O que quer fazer?</h1>
-    <a href="create_time.php">Cadastrar time</a>
-    <a href="create_jogador.php">Cadastrar jogador</a>
-    <a href="create_partida.php">Cadastrar partida</a>
+    <a href="create_time.php" id="link">Cadastrar time</a>
+    <a href="create_jogador.php" id="link">Cadastrar jogador</a>
+    <a href="create_partida.php" id="link">Cadastrar partida</a>
     <br><br>
-    <a href="read_time.php">Listar times</a>
-    <a href="read_jogador.php">Listar jogador</a>
-    <a href="read_partida.php">Listar partidas</a>
+    <a href="read_time.php" id="link">Listar times</a>
+    <a href="read_jogador.php" id="link">Listar jogador</a>
+    <a href="read_partida.php" id="link">Listar partidas</a>
 </body>
 </html>
