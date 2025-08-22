1f05a79d44c76305ccef16f4803bf247b4dad9f3
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-08-22 11:38:51 -0300

    add paginas v2

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicial</title>
 </head>
 <body>
-    
+    <h1>O que quer fazer?</h1>
+    <a href="create_jogador.php">Cadastrar jogador</a>
+    <a href="create_time.php">Cadastrar time</a>
+    <a href="create_partida.php">Cadastrar partida</a>
+    <a href="read_time.php">Listar times</a>
+    <a href="read_jogador.php">Listar jogador</a>
+    <a href="read_partida.php">Listar partidas</a>
 </body>
 </html>
